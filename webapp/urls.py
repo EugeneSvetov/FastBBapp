c63e73bee0738a407a94e6ae6d163b8c8301d50c
EugeneSvetov <audiofaun896373@gmail.com>
2022-11-18 20:19:30 +0300

    initial commit

--- webapp/urls.py
+++ webapp/urls.py
@@ -10,5 +10,8 @@ urlpatterns = [
     path('groups/', groups, name='groups'),
     path('users/', users, name='users'),
     path('api-keys/', api_keys, name='api-keys'),
-
+    path('download_file/', download_file, name='download_file'),
+    path('export_file/', export_file, name='export_file'),
+    path('delete_file/', delete_file, name='delete_file'),
+    path('create_file/',create_file,name='create_file')
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def home(request):
@@ -7,28 +7,61 @@ def home(request):
 
 
 def docs(request):
-    ...
+    list = ['еблан?']
+    data = {"header":'Документы',
+            "list":list}
+    return render(request, 'base.html', context=data)
 
 
 def temp(request):
-    ...
+    list = []
+    data = {"header": 'Шаблоны',
+            "list":list}
+    return render(request, 'base.html', context=data)
 
 
 def reports(request):
-    ...
+    list = []
+    data = {"header": "Отчеты",
+            "list":list}
+    return render(request, 'base.html', context=data)
 
 
 def data(request):
-    ...
+    list = []
+    data = {"header": 'Источники данных',
+            "list":list}
+    return render(request, 'base.html', context=data)
 
 
 def groups(request):
-    ...
+    list = []
+    data = {"header": 'Группы',
+            "list":list}
+    return render(request, 'base.html', context=data)
 
 
 def users(request):
-    ...
+    list = []
+    data = {"header": 'Пользователи',
+            "list":list}
+    return render(request, 'base.html', context=data)
 
 
 def api_keys(request):
+    list = []
+    data = {"header": 'API-ключи',
+            "list":list}
+    return render(request, 'base.html', context=data)
+
+def download_file(request):
+    ...
+
+def export_file(request):
+    ...
+
+def delete_file(request):
+    ...
+
+def create_file(request):
     ...
